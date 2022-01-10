6fb9118651bcaa1ea212f5bab2db64e5db94033c
Víctor Várez <kaliayev@protonmail.com>
2022-01-10 11:33:44 +0100

    use webpack config in container

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -7,19 +7,20 @@ const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: 8080,
     historyApiFallback: {
       index: "index.html",
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./MarketingApp": "./src/bootstrap",
+      name: "container",
+      remotes: {
+        // la sintaxis de los remotes es tal que las keys son los nombres de los diferentes módulos que queremos importar en nuestro contenedor
+        // los values van a ser los directorios donde el remoteEntry file para esos modulos
+        marketing: "marketing@http://localhost:8081/remoteEntry.js", // este marketing@ es la clave que debe coincidir con el name de nuestro micro
       },
-      // shared: ["react", "react-dom"], // dependencies that we want to share between micros
+      // shared: ["react", "react-dom"],
       shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current); // de este modo cargamos cualquier micro construido con el framework que sea, de forma transparente, en nuestro div
+  }, []);
+
+  return <div ref={ref} />;
+};
