1580e012aea646ab93dbdef32550292dd2f8c078
Víctor Várez <kaliayev@protonmail.com>
2022-01-15 22:01:48 +0100

    fix in workflow config

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -5,7 +5,6 @@ export default () => {
   const ref = useRef(null);
 
   useEffect(() => {
-    console.log(ref);
     mount(ref.current); // de este modo cargamos cualquier micro construido con el framework que sea, de forma transparente, en nuestro div
   }, []);
 
