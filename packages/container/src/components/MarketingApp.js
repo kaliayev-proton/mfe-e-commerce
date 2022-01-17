620792c3444b2cbbf39aded5b195104d367e777f
Víctor Várez <kaliayev@protonmail.com>
2022-01-17 09:50:43 +0100

    Add invalidation step to build

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -5,6 +5,7 @@ export default () => {
   const ref = useRef(null);
 
   useEffect(() => {
+    console.log(ref);
     mount(ref.current); // de este modo cargamos cualquier micro construido con el framework que sea, de forma transparente, en nuestro div
   }, []);
 
