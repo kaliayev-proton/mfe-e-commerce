e7eba100ce6d67843683414b3a30c94a30df1b39
Víctor Várez <kaliayev@protonmail.com>
2022-01-20 17:08:44 +0100

    Routing between micros

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current); // de este modo cargamos cualquier micro construido con el framework que sea, de forma transparente, en nuestro div
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // with nextPathname we are destructuring and renaming the field pathname
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    }); // de este modo cargamos cualquier micro construido con el framework que sea, de forma transparente, en nuestro div
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
