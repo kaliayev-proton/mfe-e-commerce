aaa4460454c5db9cddd4d9297156b755afef5a96
Víctor Várez <kaliayev@protonmail.com>
2022-01-21 10:16:27 +0100

    add routing and auth micro

---
+++ packages/auth/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history }) => {
+  return (
+    <div>
+      <h1>Hi!!</h1>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/auth/signin" component={SignIn} />
+            <Route path="/auth/signup" component={SignUp} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
